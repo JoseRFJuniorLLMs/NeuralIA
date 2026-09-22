0a8076e3d8e077d21e64a46a4b62f62449b1b2dc
Jose RF Junior <web2ajax@gmail.com>
2026-09-22 01:18:15 -0300

    style: apply rustfmt import wrapping

--- crates/neural-app/src/windows_app.rs
+++ crates/neural-app/src/windows_app.rs
@@ -54,10 +54,9 @@ use windows_sys::Win32::{
             GetForegroundWindow, GetParent, GetWindowTextLengthW, GetWindowTextW,
             GetWindowThreadProcessId, IDYES, IsZoomed, MB_ICONINFORMATION, MB_OK, MB_YESNO,
             MF_SEPARATOR, MF_STRING, MessageBoxW, SW_HIDE, SW_SHOW, SWP_NOACTIVATE, SWP_NOZORDER,
-            SendMessageW,
-            SetParent, SetWindowPos, SetWindowTextW, ShowWindow, TPM_RETURNCMD, TPM_RIGHTBUTTON,
-            TrackPopupMenu, WM_KEYDOWN, WS_CHILD, WS_EX_NOACTIVATE, WS_EX_TOOLWINDOW, WS_POPUP,
-            WS_TABSTOP, WS_VISIBLE,
+            SendMessageW, SetParent, SetWindowPos, SetWindowTextW, ShowWindow, TPM_RETURNCMD,
+            TPM_RIGHTBUTTON, TrackPopupMenu, WM_KEYDOWN, WS_CHILD, WS_EX_NOACTIVATE,
+            WS_EX_TOOLWINDOW, WS_POPUP, WS_TABSTOP, WS_VISIBLE,
         },
     },
 };
